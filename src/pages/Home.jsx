1921a974043d709a0888cc87a29c125698c3c473
Teodoro-Kerlegand <eddstownavy@gmail.com>
2026-01-31 15:54:20 -0600

    Initial LinguaBridge setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "3rem", textAlign: "center" }}>
+      <h1>LinguaBridge</h1>
+      <p>Learn Spanish with teachers from Mexico 🇲🇽</p>
+      <button>Book a demo class</button>
+    </main>
+  );
+}
